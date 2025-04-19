001bdc7e266df8f1f64d4468e50669af0e49f92c
ba-brown <brianbrownwa@gmail.com>
2025-04-18 17:35:06 -0700

    Completed Part 2
    Modified constructor to accept a .sdk file and loaded them into the board array.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,8 +1,19 @@
+import java.io.*;
+import java.util.*;
+
 public class SudokuBoard {
    private char[][] board;
    
-   public SudokuBoard(String fileName) {
-   
+   public SudokuBoard(String fileName) throws FileNotFoundException {
+      board = new char[9][9];
+      String input = "";
+      Scanner file = new Scanner(new File(fileName));
+      for(int r = 0; r < 9; r++) {
+         input = file.nextLine();
+         for(int c = 0; c < 9; c++) {
+            board[r][c] = input.charAt(c);
+         }
+      }
    }
    
    public String toString() {
